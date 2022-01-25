857d99de3fba0052f82ac878ff2514e40651805f
Neal Fultz <nfultz@gmail.com>
2022-01-25 16:29:02 +0000

    packaging

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author_email='neal@njnm.co',
     description='Description of my package',
     package_dir={"eonacs": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=['eonacs', 'eonacs.common'],
     python_requires='>=3.7',
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
