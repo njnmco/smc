1a5da91c8587c7a5c857504c926bb17fb388d0ca
Neal Fultz <nfultz@gmail.com>
2022-03-20 18:07:12 +0000

    update to onet 26.2

--- src/fetch/onet.sh
+++ src/fetch/onet.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/bash
 
-wget -nc https://www.onetcenter.org/dl_files/database/db_26_1_mysql.zip -P data
+wget -nc https://www.onetcenter.org/dl_files/database/db_26_2_mysql.zip -P data
 
-unzip -n data/db_26_1_mysql.zip -d data/onet_26_1
+unzip -n data/db_26_2_mysql.zip -d data/onet_26_2
 
-for f in $(find data/onet_26_1 -name '*.sql'); do
+for f in $(find data/onet_26_2 -name '*.sql' | sort); do
     echo $f...
-    sqlite3 data/onet.sqlite3 ".read $f"
+    sqlite3 /tmp/onet.sqlite3 ".read $f"
 done
+
+ mv /tmp/onet.sqlite3 data/
 
 
 #function extract_href() {
